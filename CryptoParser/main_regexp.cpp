398314035fb15b8e5b9610d6381c9dd3d8443551
Uzdenov Karim <mrkarim777@gmail.com>
2019-10-05 15:32:13 +0300

    Dalited koments and heders

--- CryptoParser/main_regexp.cpp
+++ CryptoParser/main_regexp.cpp
@@ -1,46 +1,12 @@
 #include"BaseParser.hpp"
-#include<map>
 #include<iostream>
 #include"BinaryTree.hpp"
 #define DEBUG
 using std::vector;
 using std::string;
-//using std::regex;
-//using std::smatch;
-//using std::map;
-using std::multimap;
 constexpr auto Expr_1_parser = "\\Socket\\s([a-zA-Z0-9_-]{1,})";
 constexpr auto Expr_2_parser = ".Send\\((.*?)\\)";
 
-//void insert_parse(std::string str) {
-//	auto expr_1 =  parse_1(str);
-//	for (auto&& var : expr_1) {
-//		if (var.second.length()> 0) {
-//			Tree::Brunch* main_tree = nullptr;
-//			Tree::add(var.second, main_tree);
-//			std::cout << "sock is founded" << std::endl;
-//			auto test_2expresion = parse_2(var.second,str);
-//			for (auto&& var : test_2expresion) {
-//				if (var.second.length() > 0){
-//					Tree::add(var.second,main_tree);
-//				}
-//			}
-//#ifdef DEBUG
-//			//print_map(test_2expresion);
-//
-//#endif // DEBUG
-//
-//			Tree::print(main_tree);
-//			Tree::FreeTree(main_tree);
-//		}
-//		else
-//		{
-//			std::cout << "str is null" << std::endl;
-//			return;
-//		}
-//	}
-//	
-//}
 int main()
 {
 
@@ -50,22 +16,6 @@ int main()
 	BaseParser Parse;
 	//Parse.parse(cs_text_impl_sock);
 	Parse.parse(cs_text_test);
-	//insert_parse(cs_text_impl_sock);
-	/*auto var = parse_1(cs_text_impl_sock);
-	for (auto&& varible : var)
-	{
-		std::cout << varible.first << " : " << varible.second << std::endl;
-	}
-
-	Tree::Brunch* tree = nullptr;
-	Tree::add(sometext, tree);
-	Tree::print(tree);
-	Tree::FreeTree(tree);*/
-
-
-	//printer::print_map(res);
-
-	//printer::pause();
 
 	return 0;
 	
